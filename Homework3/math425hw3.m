9b333d54114499c1831d1cf5e624273ab7f4f5d4
Bryan Lee <62684188+BryanL43@users.noreply.github.com>
2024-10-02 15:08:44 -0700

    HW 3 corrections

--- Homework3/math425hw3.m
+++ Homework3/math425hw3.m
@@ -1,29 +1,14 @@
-% Exercise 1
-% 1a
-fprintf("Exercise 1a:\n");
-v = rand(5, 1);
-w = rand(3, 1);
-A = v * w';
-disp(A);
-fprintf("The rank of A is %d\n", rank(A));
-
-
 % Exercise 2
 fprintf("\n");
 fprintf("Exercise 2:\n");
 v1 = [1; 2; 0; 1];
 v2 = [0; -1; 3; 0];
 v3 = [2; 0; 1; -1];
-v4 = [3; 0; -1; -2];
-V = [v1 v2 v3 v4];
-disp(V);
-
-[n, k] = size(V);
-if (rank(V) == k)
-    fprintf("The vectors are linearly independent and not a linear combination of each other.\n");
-elseif (rank(V) < k)
-   fprintf("The vectors are linear dependent/combination of each other.\n");
-end
+b = [3; 0; -1; -2];
+A = [v1 v2 v3 b];
+disp(rref(A));
+disp("Taking the rref of the augmented matrix A = [ v1 v2 v3 | b ], we can see that the last row has 0s beside the b column.");
+disp("This means that the matrix is inconsistent and b is not a linear combination of v1, v2, and v3.");
 
 
 % Exercise 3
@@ -41,16 +26,27 @@ if (rank(A) == 3)
 else
     fprintf("The vectors v1, v2, v3, v4 do NOT span R^3.\n");
 end
-fprintf("Since the rank(A) not equals to 3, we have only a span of R^2 due to v3/v4 being\n" + ...
-    "a combination of v1 and v2.\n")
+fprintf("Since the rank of A does not equals to 3, but instead it is %d.\n" + ...
+    "the vectors spans R^%d.\n", rank(A), rank(A));
+disp(rref(A));
+fprintf("Taking the rref of matrix A also shows that v3 and v4 are a combination of\n" + ...
+    "v1 and v2, where v3 = (-1 * v1) + v2 and v4 = v1 + v2.\n");
 
 fprintf("\nExercise 3b:\n");
-A = [v1 v2 v3 v4];
-augmented_A = [A zeros(size(A, 1), 1)];
-disp(rref(augmented_A));
-fprintf("Here we can see that rref(A) has a free variable, thus not linear independent.\n");
+disp(rref(A));
+fprintf("Here we can see that rref(A) has a free variable (shown by the last row with all 0s), thus not linear independent.\n");
 fprintf("Additionally, according to the textbook Lemma 2.23 (pg. 96), it states that if\n" + ...
     "any collection of k (column vectors) > n (row) vectors in R^n then the vectors are linearly dependent.\n");
+
+fprintf("\nExercise 3c:\n");
+fprintf("No, v1, v2, v3, v4 do not form a basis for R^3. Since the vectors only span R^2 as shown in 3a\n" + ...
+    "and aren't linearly independent as shown in 3b, then we cannot form a basis of R^3.\n");
+fprintf("Additionally, it is also not possible to choose some subset which is a basis, as the span of the\n" + ...
+    "vectors is only R^2.\n");
+
+fprintf("\nExercise 3d:\n");
+fprintf("The dimension of the span of v1, v2, v3, v4 is 2. As explained in 3a, there are only 2 linearly independent\n" + ...
+    "vectors in the set, shown by the rank(A) = 2.\n");
 
 
 % Exercise 4
